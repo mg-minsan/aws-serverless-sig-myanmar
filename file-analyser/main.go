9dd3efcaed5240869c8747b57c3371e2aca3369f
minsan <andes@waypacific.sg>
2020-06-22 21:46:37 +0800

    file analyser function

---
+++ file-analyser/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+type Item struct{
+  Key string
+  Timestamp string
+}
+func main(){
+  lambda.Start(handler)
+}
+
+func handler(ctx context.Context, event events.S3Event){
+  sess := session.Must(session.NewSessionWithOptions(session.Options{
+    SharedConfigState: session.SharedConfigEnable,
+  }))
+  /*
+  svc := dynamodb.New(sess, &aws.Config{
+    Region:   aws.String("us-west-2"),
+    Endpoint: aws.String("http://dynamodb-local:8000"),
+  })
+  */
+
+  svc := dynamodb.New(sess)
+  for _, record := range event.Records {
+    s3 := record.S3
+    now := time.Now().String()
+    item := Item{
+      Key: s3.Object.Key,
+      Timestamp: now,
+    }
+    av, err := dynamodbattribute.MarshalMap(item)
+    if err != nil {
+      log.Println("Got error marshalling new movie item:")
+    }
+    input := &dynamodb.PutItemInput{
+      Item:      av,
+      TableName: aws.String("FileMetadata"),
+    }
+    _, err = svc.PutItem(input)
+    if err != nil{
+      log.Print(err)
+    }
+    log.Print("Uploaded")
+  }
+}
+
